12cdee15aff09b19f5565c50f455adba675e52e4
Edgar Vintimill <edgar@lugamio.com>
2023-09-16 11:29:02 -0500

    Fix react component

--- frontend/src/components/FirstComponent.js
+++ frontend/src/components/FirstComponent.js
@@ -6,7 +6,7 @@ function FirstComponent() {
             <div
                 className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
                 <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                    This is a REACT component. !!2ddd-999 hola ggg mundo oo
+                    This is a REACT component.
                 </h2>
                 <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
                     DJSTARTER is integrated with react
